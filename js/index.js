44ab36b0a92cb99ee05d6016323cab5e9b94b02c
Anja-Klassen <anja.klassen@posteo.de>
2024-10-21 10:40:56 +0200

    add solution and comments

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
+//Diese Funktion versteckt das Fehler-Element (tosError), indem das hidden-Attribut hinzugefügt wird. Das bedeutet, wenn diese Funktion aufgerufen wird, wird die Fehlermeldung ausgeblendet.
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
 
+//Diese Funktion zeigt das Fehler-Element (tosError) an, indem das hidden-Attribut entfernt wird. Wenn der Benutzer die Checkbox nicht aktiviert hat, wird diese Funktion aufgerufen, um den Fehler anzuzeigen.
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
@@ -16,7 +18,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  // prüft, ob die Checkbox nicht markiert- Wenn ja Codeblock: Fehler wird gezeigt und Funktion wird über return sofort beendet. Rest inklusive alert wird nicht ausgeführt.
+  if (!tosCheckbox.checked){
+    showTosError();
+    return;
+   } 
+// wenn Checkbox markiert, wird die Fehlermeldung ausgeblendet und alert wird ausgeführt
+   hideTosError();
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
